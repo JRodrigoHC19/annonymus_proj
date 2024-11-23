b83f61cb7dd597448893ec3280a95732ea9b8c93
JRodrigoHC19 <jaime.huacac@tecsup.edu.pe>
2024-11-22 19:57:59 -0500

    v2 final

--- env.ts
+++
@@ -1,3 +0,0 @@
-export const environment = { 
-    api_url: 'http://52.53.235.234:3000/api/users/login'
-};
--- script.sh
+++ script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-git clone https://github.com/JRodrigoHC19/annonymus_proj.git project
+IP_AUTH=54.67.101.141
 
+git clone https://github.com/JRodrigoHC19/annonymus_proj.git project
 cd project
 
+
+echo "export const environment = { api_url: "http://$IP_AUTH:3000/api/users/login" };" > env.ts
 docker-compose up -d
+
 
 echo "Servicios desplegados"
