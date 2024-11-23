4766d60290579076c7d020614f58db391938ef99
JRodrigoHC19 <jaime.huacac@tecsup.edu.pe>
2024-11-22 20:11:42 -0500

    v3 final nomas

--- script.sh
+++ script.sh
@@ -5,9 +5,8 @@ IP_AUTH=54.67.101.141
 git clone https://github.com/JRodrigoHC19/annonymus_proj.git project
 cd project
 
-
 echo "export const environment = { api_url: "http://$IP_AUTH:3000/api/users/login" };" > env.ts
-docker-compose up -d
 
+docker-compose up -d
 
 echo "Servicios desplegados"
